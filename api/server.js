5cc9e986f8f6631e2ec38796050fa212b8e824c1
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-11-16 14:06:44 -0700

    going to the root url responds by saying that the api is up

--- api/server.js
+++ api/server.js
@@ -10,6 +10,10 @@ server.use(cors())
 
 server.use("/api/users", usersRouter);
 
+server.get("/", (req, res) => {
+  res.status(200).json({message: "api is up"});
+})
+
 server.use((req, res) => res.status(404).json({message:"Resource not found", method:req.method, path:req.url}));
 
 module.exports = server
