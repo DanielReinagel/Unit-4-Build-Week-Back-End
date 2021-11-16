59dba5332ccaee39881dff6e83f2b397031dbd10
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-11-16 13:47:36 -0700

    typo fix

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const helmet = require('helmet')
-const cors = require('cors')|
+const cors = require('cors');
 const usersRouter = require('./users/users-router');
 
 const server = express()
